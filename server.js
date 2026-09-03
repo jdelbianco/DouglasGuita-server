8e35c721d2df80a61f0a94f1f69e1e6d440716fb
jdelbianco <j.belbianco@outlook.com>
2026-09-03 12:33:08 -0300

    Add CORS

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors'; // 1. Importe o CORS
 // Veja que agora adicionamos o ./src/ no caminho das duas rotas!
 import userRoutes from './src/routes/userRoutes.js';
 import taskRoutes from './src/routes/taskRoutes.js';
@@ -7,7 +8,9 @@ import backingTrackRoutes from './src/routes/backingTrackRoutes.js';
 import loginRoute from './src/routes/loginRoute.js';
 
 
+
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use('/user', userRoutes);
